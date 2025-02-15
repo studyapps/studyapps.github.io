edbfe3d38b9f083cbc4a559da8e56395fb64b327
studyapps <ashikaga@u-fukui.ac.jp>
2025-02-16 00:14:24 +0900

    Add files via upload

--- test.mjs
+++ test.mjs
@@ -2,14 +2,15 @@ class Test {
   constructor() {}
   getInfo() { // 拡張機能の各種情報
     return {
-      id: 'test',
-      name: 'Test', // 拡張機能の名前
+      id: 'sheet_music',
+      name: '楽譜', // 拡張機能の名前
       blocks: [ // 各ブロックの定義
         {
           opcode: 'hello', // このブロックが実行されると、クラス内のhelloというメソッドが呼ばれることを意味しています
           blockType: Scratch.BlockType.COMMAND,　// 「10歩動かす」のような通常の命令ブロック
           text: 'hello' // ブロックに表示されるテキスト
         }
+
       ]
     }
   }
