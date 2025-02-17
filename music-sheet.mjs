b82554c4517bd00fb0f94203b32f340d95efc017
studyapps <ashikaga@u-fukui.ac.jp>
2025-02-17 09:36:22 +0900

    Add files via upload

---
+++ music-sheet.mjs
@@ -0,0 +1,20 @@
+class music-sheet {
+  constructor() {}
+  getInfo() { // 拡張機能の各種情報
+    return {
+      id: 'music-sheet',
+      name: 'music-sheet', // 拡張機能の名前
+      blocks: [ // 各ブロックの定義
+        {
+          opcode: 'hello', // このブロックが実行されると、クラス内のhelloというメソッドが呼ばれることを意味しています
+          blockType: Scratch.BlockType.COMMAND,　// 「10歩動かす」のような通常の命令ブロック
+          text: 'hello' // ブロックに表示されるテキスト
+        }
+      ]
+    }
+  }
+  hello() {
+    console.log('hello'); // console log に hello と出力
+  }
+}
+Scratch.extensions.register(new music-sheet());
