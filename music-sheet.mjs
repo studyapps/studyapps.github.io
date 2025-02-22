fe17f17cbbfd1bcb34fd98771bfc9e72c2948f80
studyapps <ashikaga@u-fukui.ac.jp>
2025-02-22 17:38:07 +0900

    Add files via upload

--- music-sheet.mjs
+++ music-sheet.mjs
@@ -38,19 +38,19 @@ class MusicSheet {
                 {
                     opcode: 'getCValue',
                     blockType: Scratch.BlockType.REPORTER,
-                    text: 'Cブロック (いA × B)'
+                    text: 'Cブロック (うA × B)'
                 }
             ],
             menus: {
                 aMenu: [
-                    { text: '1', value: '1' },
-                    { text: '2', value: '2' },
-                    { text: '3', value: '3' }
+                    { text: '1', value: 1 },
+                    { text: '2', value: 2 },
+                    { text: '3', value: 3 }
                 ],
                 bMenu: [
-                    { text: '4', value: '4' },
-                    { text: '5', value: '5' },
-                    { text: '6', value: '6' }
+                    { text: '4', value: 4 },
+                    { text: '5', value: 5 },
+                    { text: '6', value: 6 }
                 ]
             }
         };
