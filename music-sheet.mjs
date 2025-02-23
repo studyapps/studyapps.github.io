ef13ab80f09898bbb3acecbd762f1c8d63718097
studyapps <ashikaga@u-fukui.ac.jp>
2025-02-23 16:44:28 +0900

    Add files via upload

--- music-sheet.mjs
+++ music-sheet.mjs
@@ -29,7 +29,7 @@ class CustomExtension {
                         },
                         TEMPO:{
                             type: Scratch.ArgumentType.NUMBER,
-                            defaultValue: this.temponoteValue, // 初期値
+                            defaultValue: this.speedValue, // 初期値
                         }  
                     }
                 },
