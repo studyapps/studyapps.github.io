3febdb72d80395adce1c1cbdd570289da5709b80
studyapps <ashikaga@u-fukui.ac.jp>
2025-02-24 09:05:54 +0900

    Add files via upload

--- music-sheet.mjs
+++ music-sheet.mjs
@@ -2,9 +2,9 @@ class CustomExtension {
     constructor(runtime) {
         this.runtime = runtime;
         this.tempoValue = 120; // 初期値
-        this.temponoteValue = 1/4; // 初期値
-        this.noteValue = 1/4; // 初期値
-        this.restValue = 1/4; // 初期値
+        this.temponoteValue = 4; // 初期値
+        this.noteValue = 4; // 初期値
+        this.restValue = 4; // 初期値
         this.periodValue = 60 / this.tempoValue / this.temponoteValue;
         this.scaleValue = 60;
     }
@@ -12,7 +12,7 @@ class CustomExtension {
     getInfo() {
         return {
             id: 'customExtension',
-            name: 'toio MUSIC5',
+            name: 'toio MUSIC6',
             color1: '#000000', // ブロックのメインカラー
             color2: '#000000', // ブロックの枠線や影の色
             blocks: [
@@ -23,7 +23,7 @@ class CustomExtension {
                     arguments: {
                         NOTE:{
                             type: Scratch.ArgumentType.STRING,
-                            defaultValue: 1/4, // 初期値
+                            defaultValue: 4, // 初期値
                             menu: 'noteMenu'
                         },
                         TEMPO:{
@@ -44,7 +44,7 @@ class CustomExtension {
                     arguments: {
                         NOTE: {
                             type: Scratch.ArgumentType.STRING,
-                            defaultValue: 1/4, // 初期値
+                            defaultValue: 4, // 初期値
                             menu: 'noteMenu'
                         }
                     }
@@ -56,7 +56,7 @@ class CustomExtension {
                     arguments: {
                         REST: {
                             type: Scratch.ArgumentType.STRING,
-                            defaultValue: 1/4, // 初期値
+                            defaultValue: 4, // 初期値
                             menu: 'restMenu'
                         }
                     }
@@ -77,33 +77,62 @@ class CustomExtension {
             menus: {
                 noteMenu: [
                     { text: '𝅝', value: 1},
-                    { text: '𝅗𝅥', value: 1/2},
-                    { text: '𝅘𝅥', value: 1/4},
-                    { text: '𝅘𝅥𝅮', value: 1/8},
-                    { text: '𝅘𝅥𝅯', value: 1/16},
-                    { text: '𝅘𝅥𝅰', value: 1/32},
-                    { text: '𝅘𝅥𝅱', value: 1/64},
-                    { text: '𝅘𝅥𝅲', value: 1/128}
+                    { text: '𝅗𝅥', value: 2},
+                    { text: '𝅘𝅥', value: 4},
+                    { text: '𝅘𝅥𝅮', value: 8},
+                    { text: '𝅘𝅥𝅯', value: 16},
+                    { text: '𝅘𝅥𝅰', value: 32},
+                    { text: '𝅘𝅥𝅱', value: 64},
+                    { text: '𝅘𝅥𝅲', value: 128}
                 ],
                 restMenu: [
                     { text: '𝄻', value: 1},
-                    { text: '𝄼', value: 1/2},
-                    { text: '𝄽', value: 1/4},
-                    { text: '𝄾', value: 1/8},
-                    { text: '𝄿', value: 1/16},
-                    { text: '𝅀', value: 1/32},
-                    { text: '𝅁', value: 1/64},
-                    { text: '𝅂', value: 1/128} 
+                    { text: '𝄼', value: 2},
+                    { text: '𝄽', value: 4},
+                    { text: '𝄾', value: 8},
+                    { text: '𝄿', value: 16},
+                    { text: '𝅀', value: 32},
+                    { text: '𝅁', value: 64},
+                    { text: '𝅂', value: 128} 
                 ],
                 scaleMenu: [
-                    {text: 'ド', value: 60},
-                    {text: 'レ', value: 62},
-                    {text: 'ミ', value: 64},
-                    {text: 'ファ', value: 65},
-                    {text: 'ソ', value: 67},
-                    {text: 'ラ', value: 69},
-                    {text: 'シ', value: 71},
-                    {text: 'ド\'', value: 72}
+                    {text: 'ド\,', value: '48'},
+                    {text: '#ド\,', value: '49'},
+                    {text: 'レ\,', value: '50'},
+                    {text: '#レ\,', value: '51'},
+                    {text: 'ミ\,', value: '52'},
+                    {text: 'ファ\,', value: '53'},
+                    {text: '#ファ\,', value: '54'},
+                    {text: 'ソ\,', value: '55'},
+                    {text: '#ソ\,', value: '56'},
+                    {text: 'ラ\,', value: '57'},
+                    {text: '#ラ\,', value: '58'},
+                    {text: 'シ\,', value: '59'},
+                    {text: 'ド', value: '60'},
+                    {text: '#ド', value: '61'},
+                    {text: 'レ', value: '62'},
+                    {text: '#レ', value: '63'},
+                    {text: 'ミ', value: '64'},
+                    {text: 'ファ', value: '65'},
+                    {text: '#ファ', value: '66'},
+                    {text: 'ソ', value: '67'},
+                    {text: '#ソ', value: '68'},
+                    {text: 'ラ', value: '69'},
+                    {text: '#ラ', value: '70'},
+                    {text: 'シ', value: '71'},
+                    {text: 'ド\'', value: '72'}
+                    {text: '#ド\'', value: '73'},
+                    {text: 'レ\'', value: '74'},
+                    {text: '#レ\'', value: '75'},
+                    {text: 'ミ\'', value: '76'},
+                    {text: 'ファ\'', value: '77'},
+                    {text: '#ファ\'', value: '78'},
+                    {text: 'ソ\'', value: '79'},
+                    {text: '#ソ\'', value: '90'},
+                    {text: 'ラ\'', value: '91'},
+                    {text: '#ラ\'', value: '92'},
+                    {text: 'シ\'', value: '93'},
+                    {text: 'ド\'\'', value: '94'}
                 ]
             }
         };
@@ -113,11 +142,11 @@ class CustomExtension {
         return this.tempoValue;
     }
     chooseNote(args) {
-        this.noteValue = parseFloat(args.NOTE);
+        this.noteValue = 1 / parseFloat(args.NOTE);
         return this.noteValue * this.periodValue;
     }
     chooseRest(args) {
-        this.restValue = parseFloat(args.REST);
+        this.restValue = 1 / parseFloat(args.REST);
         return this.restValue * this.periodValue;
     }
     chooseScale(args) {
