3c77edd1c671fec618928a25caf50e264795312e
studyapps <ashikaga@u-fukui.ac.jp>
2025-02-25 11:26:17 +0900

    Add files via upload

--- music-sheet.mjs
+++ music-sheet.mjs
@@ -8,8 +8,7 @@ class CustomExtension {
         this.periodValue = 60 / this.tempoValue / this.temponoteValue; //１小節の時間
         this.scaleValue = '60'; //ドレミ
         this.chapterValue = ''; //現在実行中のチャプター
-        
-        this.runtime.on('PROJECT_STOP_ALL', () => { }); // 停止ボタン
+        // this.runtime.on('PROJECT_STOP_ALL', () => { }); // 停止ボタン
     }
 
     getInfo() {
