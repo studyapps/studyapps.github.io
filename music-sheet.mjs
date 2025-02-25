1948006f3ff0fc006dc9c133ee34a765a5d47d9a
studyapps <ashikaga@u-fukui.ac.jp>
2025-02-25 14:50:25 +0900

    Add files via upload

--- music-sheet.mjs
+++ music-sheet.mjs
@@ -1,5 +1,6 @@
 class CustomExtension {
     constructor(runtime) {
+        this.runtime.on('PROJECT_STOP_ALL', chapterValue = '1');
         this.runtime = runtime;
         this.tempoValue = '120'; // テンポの数値
         this.temponoteValue = '1'; // テンポの基準となる音符
@@ -8,14 +9,14 @@ class CustomExtension {
         this.periodValue = 60 / this.tempoValue / this.temponoteValue; //１小節の時間
         this.scaleValue = '60'; //ドレミ
         this.chapterValue = ''; //現在実行中のチャプター
-        this.runtime.on('PROJECT_STOP_ALL', chapterValue = '');
+
         // this.runtime.on('PROJECT_START', ); // 旗ボタンが押されたときに変数をリセット
     }
 
     getInfo() {
         return {
             id: 'customExtension',
-            name: 'MUSIC8',
+            name: 'MUSIC',
             color1: '#000000', // ブロックのメインカラー
             color2: '#000000', // ブロックの枠線や影の色
             blocks: [
