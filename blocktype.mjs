493212e04cd49ac4286ece2dae5df2c41f4e6f34
studyapps <ashikaga@u-fukui.ac.jp>
2025-03-04 14:17:09 +0900

    Add files via upload

--- blocktype.mjs
+++ blocktype.mjs
@@ -1,24 +1,68 @@
-class TestExtension {
-    getInfo() {
-        return {
-            id: 'testExtension',
-            name: 'Test Extension',
-            blocks: [
-                {
-                    opcode: 'addComment',
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: 'コメントを追加'
-                }
-            ]
-        };
+class CustomExtension {
+    constructor(runtime) {
+        this.runtime = runtime;
+        this.block = 'trial'; // 初期値を'trial'に設定
     }
 
-    addComment() {
-        const workspace = Scratch.vm.runtime._editingTarget.sprite.clones[0].customBlocks.workspace;
-        if (workspace) {
-            workspace.addComment("テスト", 10, 10);
-        }
+    getInfo() {
+        var c = {
+            id: 'BlockType',
+            name: 'URL Extension',
+            color1: "#A6A6A6",
+            blocks: [],
+            menus: {
+                blockTypeMenu: {
+                    acceptReporters: true,
+                    items: ['Basic', 'trial']
+                }
+            }
+        };
+        return c.blocks.push(
+            {
+                opcode: 'setBlockType',
+                blockType: Scratch.BlockType.COMMAND,
+                text: 'BlockTypeを [TYPE] に設定',
+                arguments: {
+                    TYPE: {
+                        type: Scratch.ArgumentType.STRING,
+                        menu: 'blockTypeMenu'
+                    }
+                }
+            },
+            {
+                opcode: 'getBlockType',
+                blockType: Scratch.BlockType.REPORTER,
+                text: 'BrockType'
+            },
+            {
+                opcode: 'getTrialBlock',
+                blockType: Scratch.BlockType.REPORTER,
+                text: 'trialのブロック'
+            }
+        ), this.shouldShowBasicBlocks() && c.blocks.push(
+            {
+                opcode: 'getBasicBlock',
+                blockType: Scratch.BlockType.REPORTER,
+                text: 'Basicのブロック'
+            }
+        ), c;
     }
+
+    setBlockType(args) {
+        this.block = args.TYPE;
+    }
+    getBlockType() {
+        return this.block;
+    }
+    getBasicBlock() {
+    }
+
+    getTrialBlock() {
+    }
+
+    shouldShowBasicBlocks() {
+        return this.block == "basic";
+      }
 }
 
-Scratch.extensions.register(new TestExtension());
+Scratch.extensions.register(new CustomExtension());
