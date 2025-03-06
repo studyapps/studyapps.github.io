69d1b3ce6abe8e9c40115209baf97c5ee4b99615
studyapps <ashikaga@u-fukui.ac.jp>
2025-03-06 11:28:10 +0900

    Add files via upload

--- blocktype.mjs
+++ blocktype.mjs
@@ -19,50 +19,48 @@ class CustomExtension {
             }
         };
 
-        c.blocks.push(
-            {
-                opcode: 'setParameters',
-                blockType: Scratch.BlockType.COMMAND,
-                text: 'Block =  [TYPE] ID = [ID]',
-                arguments: {
-                    TYPE: {
-                        type: Scratch.ArgumentType.STRING,
-                        menu: 'blockTypeMenu',
-                        defaultValue: this.block ?? 'Trial'
-                    },
-                    ID: {
-                        type: Scratch.ArgumentType.STRING,
-                        defaultValue: this.id ?? "お客様ID"
-                    }
-                }
-            },
-            {
-                opcode: 'getID',
-                blockType: Scratch.BlockType.REPORTER,
-                text: 'ID'
-            },
-            {
-                opcode: 'getBlockType',
-                blockType: Scratch.BlockType.REPORTER,
-                text: 'Block'
-            }
-        );
-
-        // 5秒待ってから Basic ブロックを追加
+        // 5秒待ってから全てのブロックを追加
         setTimeout(() => {
-            if (this.shouldShowBasicBlocks()) {
-                c.blocks.push(
-                    {
-                        opcode: 'getTrialBlock',
-                        blockType: Scratch.BlockType.COMMAND,
-                        text: 'Trialライセンスで表示されるブロック'
-                    },
-                    {
-                        opcode: 'getBasicBlock',
-                        blockType: Scratch.BlockType.COMMAND,
-                        text: 'Basicライセンスで表示されるブロック'
+            c.blocks.push(
+                {
+                    opcode: 'setParameters',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'Block =  [TYPE] ID = [ID]',
+                    arguments: {
+                        TYPE: {
+                            type: Scratch.ArgumentType.STRING,
+                            menu: 'blockTypeMenu',
+                            defaultValue: this.block ?? 'Trial'
+                        },
+                        ID: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: this.id ?? "お客様ID"
+                        }
                     }
-                );
+                },
+                {
+                    opcode: 'getID',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'ID'
+                },
+                {
+                    opcode: 'getBlockType',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'Block'
+                },
+                {
+                    opcode: 'getTrialBlock',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'Trialライセンスで表示されるブロック'
+                }
+            );
+
+            if (this.shouldShowBasicBlocks()) {
+                c.blocks.push({
+                    opcode: 'getBasicBlock',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'Basicライセンスで表示されるブロック'
+                });
             }
         }, 5000);
 
